6cebd75f2500e1079f76617e4d1189f871f2811a
Dustin McQuay <dmcquay@gmail.com>
2022-04-30 15:16:04 -0600

    Updates

---
+++ precache-manifest.f81f07fdc81f7ee13356d73bdb083e5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f567ba235294f9825b1cb7b185cf21",
+    "url": "/game-scoring-apps/index.html"
+  },
+  {
+    "revision": "1ce075020aa34df1e182",
+    "url": "/game-scoring-apps/static/css/main.83771e61.chunk.css"
+  },
+  {
+    "revision": "2d40147e768e80eeb199",
+    "url": "/game-scoring-apps/static/js/2.1741594b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-scoring-apps/static/js/2.1741594b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ce075020aa34df1e182",
+    "url": "/game-scoring-apps/static/js/main.3b55448e.chunk.js"
+  },
+  {
+    "revision": "20b89638398706e38fcf",
+    "url": "/game-scoring-apps/static/js/runtime-main.549615fc.js"
+  },
+  {
+    "revision": "0db3c92e60ba779cc773ea135b76fc9f",
+    "url": "/game-scoring-apps/static/media/menu.0db3c92e.svg"
+  }
+]);
